ae34a5ac0d08dd59093706e85ac2b84f0c932b31
Liu Xinan <xinan@me.com>
2015-10-05 17:01:14 +0800

    Define constants

---
+++ src/Constants.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Xinan on 4/10/15.
+ */
+public class Constants {
+  public final static short segmentSize = 1000;
+  public final static short headerSize = 10;
+  public final static short chunkSize = 990;
+}
